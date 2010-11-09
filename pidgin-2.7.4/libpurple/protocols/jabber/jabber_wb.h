48855173265b6501487d7343ddb20df34e70e91c
dcrodjer <dcrodjer@gmail.com>
2010-11-10 01:43:26 +0530

    Session management working, General Whiteboard message sending and parsing for xmpp.

--- pidgin-2.7.4/libpurple/protocols/jabber/jabber_wb.h
+++ pidgin-2.7.4/libpurple/protocols/jabber/jabber_wb.h
@@ -32,24 +32,58 @@
 #include "cmds.h"
 #include "message.h"
 
+#define JABBER_WB_CANVAS_WIDTH  368
+#define JABBER_WB_CANVAS_HEIGHT 256
+
+/* color codes (most likely RGB) */
+#define	JABBER_WB_COLOR_RED    13369344
+#define	JABBER_WB_COLOR_ORANGE 16737792
+#define	JABBER_WB_COLOR_YELLOW 15658496
+#define	JABBER_WB_COLOR_GREEN     52224
+#define	JABBER_WB_COLOR_CYAN      52428
+#define	JABBER_WB_COLOR_BLUE        204
+#define	JABBER_WB_COLOR_VIOLET  5381277
+#define	JABBER_WB_COLOR_PURPLE 13369548
+#define	JABBER_WB_COLOR_TAN    12093547
+#define	JABBER_WB_COLOR_BROWN   5256485
+#define	JABBER_WB_COLOR_BLACK         0
+#define	JABBER_WB_COLOR_GREY   11184810
+#define	JABBER_WB_COLOR_WHITE  16777215
+
+#define PALETTE_NUM_OF_COLORS 12
+
+/* Doodle brush sizes (most likely variable) */
+#define JABBER_WB_BRUSH_SMALL   2
+#define JABBER_WB_BRUSH_MEDIUM  5
+#define JABBER_WB_BRUSH_LARGE  10
+
+#define JABBER_WB_MAX_BRUSH_MOTIONS 100
 
 typedef struct _JabberWbMessage {
 	JabberMessage *jm;
 	enum {
 			JABBER_WB_INITIATE,
 			JABBER_WB_ACCEPT,
-			JABBER_WB_UPDATE,
-			JABBER_WB_TERMINATE,
+			JABBER_WB_DRAW,
+			JABBER_WB_CLEAR,
+			JABBER_WB_END,
 		}action;
 	char *data;
 }JabberWbMessage;
 
+typedef struct _wb_session
+{
+	int brush_size;  /* Size of drawing brush */
+	int brush_color; /* Color of drawing brush */
+} wb_session;
+
+
 void handle_wb_initiate(JabberWbMessage *jwm);
 void handle_wb_accept(JabberWbMessage *jwm);
-void jabber_wb_accept(JabberWbMessage *jwm, const char *name);
 PurpleWhiteboard *jabber_wb_create(PurpleAccount *account, char *to);
+void jabber_wb_accept(JabberWbMessage *jwm, const char *name);
 void jabber_wb_initiate(PurpleConnection *gc, const char *name);
-xmlnode *jabber_wb_message_new(PurpleConnection *gc, const char *to, const char *sid);
+void jabber_wb_send_generic(PurpleConnection *gc, const char *to, const char *action, const char *data);
 PurpleConnection *jabber_wb_message_get_connection(JabberWbMessage *jwm);
 void jabber_wb_message_parse(JabberMessage *jm, xmlnode *packet);
 void jabber_wb_start(PurpleWhiteboard *wb);
