9cdd4e9f01ffabb97921f8849eafd76440ab84ca
dcrodjer <dcrodjer@gmail.com>
2010-11-10 01:43:26 +0530

    Session management working, General Whiteboard message sending and parsing for xmpp.

--- pidgin-2.7.4/libpurple/protocols/jabber/jabber_wb.c
+++ pidgin-2.7.4/libpurple/protocols/jabber/jabber_wb.c
@@ -40,29 +40,27 @@
 #include "server.h"
 #include "util.h"
 #include "version.h"
-
+#include "string.h"
 #include "jabber.h"
 #include "iq.h"
 #include "message.h"
 #include "whiteboard.h"
 #include "jabber_wb.h"
 
-void handle_wb_initiate(JabberWbMessage *jwm)
-{
-	purple_debug_info("jabber-wb","Handling Wb Initiate request\n");
-	jabber_wb_accept(jwm , jwm->jm->from);
+void handle_wb_initiate(JabberWbMessage *jwm) {
+	purple_debug_info("jabber-wb", "Handling Wb Initiate request\n");
+	jabber_wb_accept(jwm, jwm->jm->from);
 }
 
-void handle_wb_accept(JabberWbMessage *jwm)
-{
+void handle_wb_accept(JabberWbMessage *jwm) {
 	PurpleAccount *account;
 	PurpleWhiteboard *wb;
 	PurpleConnection *gc = jabber_wb_message_get_connection(jwm);
 	char *to = jwm->jm->from;
-	purple_debug_info("jabber-wb","Handling Wb Accept request\n");
+	purple_debug_info("jabber-wb", "Handling Wb Accept request\n");
 	account = purple_connection_get_account(gc);
 	wb = purple_whiteboard_get_session(account, to);
-	if(wb == NULL){
+	if (wb == NULL) {
 		wb = jabber_wb_create(account, to);
 	}
 }
@@ -72,56 +70,42 @@ void jabber_wb_accept(JabberWbMessage *jwm, const char *name)
 	PurpleAccount *account;
 	PurpleWhiteboard *wb;
 	PurpleConnection *gc = jabber_wb_message_get_connection(jwm);
-	JabberStream *js = purple_connection_get_protocol_data(gc);
-	char *sid = jwm->jm->id;
-	char *to = (char*)name;
-	xmlnode *message, *child;
+	char *to = (char*) name;
 	g_return_if_fail(gc);
 	g_return_if_fail(name);
 	account = purple_connection_get_account(gc);
 	wb = purple_whiteboard_get_session(account, to);
-	if(wb == NULL){
+	if (wb == NULL) {
 		wb = jabber_wb_create(account, to);
-		message = jabber_wb_message_new(gc, to, sid);
-		child = xmlnode_get_child(message, "whiteboard");
-		xmlnode_set_attrib(child, "action", "accept");
-		xmlnode_insert_data(child,"Test Data",10);
-		if (message){
-			jabber_send(js, message);
-		}
+		jabber_wb_send_generic(gc, to, "accept", "");
 	}
 }
 
-PurpleWhiteboard *jabber_wb_create(PurpleAccount *account, char *to)
-{
+PurpleWhiteboard *jabber_wb_create(PurpleAccount *account, char *to) {
 	PurpleWhiteboard *wb;
-	purple_debug_info("jabber-wb", "Createing a jabber whiteboard\n");
+	wb_session *wbs = g_new0(wb_session, 1);
+	purple_debug_info("jabber-wb", "Createing a new jabber whiteboard session.\n");
 	wb = purple_whiteboard_create(account, to, 0);
+	wbs->brush_size = JABBER_WB_BRUSH_MEDIUM;
+	wbs->brush_color = JABBER_WB_COLOR_CYAN;
+	wb->proto_data = wbs;
 	return wb;
 }
 
-void jabber_wb_initiate(PurpleConnection *gc, const char *name)
-{
+void jabber_wb_initiate(PurpleConnection *gc, const char *name) {
 	PurpleAccount *account;
 	PurpleWhiteboard *wb;
-	JabberStream *js = purple_connection_get_protocol_data(gc);
-	xmlnode *message, *child;
-	gchar *sid = NULL;
-	char *to = (char*)name;
+	//JabberStream *js = purple_connection_get_protocol_data(gc);
+	//gchar *sid = NULL;
+	char *to = (char*) name;
 	g_return_if_fail(gc);
 	g_return_if_fail(name);
 	account = purple_connection_get_account(gc);
 	wb = purple_whiteboard_get_session(account, to);
-	if(wb == NULL){
-		sid = jabber_get_next_id(js);
+	if (wb == NULL) {
+		//sid = jabber_get_next_id(js);
 		purple_debug_info("jabber-wb", "Initiating a new whiteboard session.\n");
-		message = jabber_wb_message_new(gc, to, sid);
-		child = xmlnode_get_child(message, "whiteboard");
-		xmlnode_set_attrib(child, "action", "initiate");
-		xmlnode_insert_data(child,"Test Data",10);
-		if (message){
-			jabber_send(js, message);
-		}
+		jabber_wb_send_generic(gc, to, "initiate", "");
 		//wb = purple_whiteboard_create(account, to);
 		/* Insert this 'session' in the list.  At this point, it's only a
 		 * requested session.
@@ -133,75 +117,84 @@ void jabber_wb_initiate(PurpleConnection *gc, const char *name)
 	 */
 }
 
-xmlnode *jabber_wb_message_new(PurpleConnection *gc, const char *to, const char *sid){
+void jabber_wb_send_generic(PurpleConnection *gc, const char *to, const char *action, const char *data)
+{
 	JabberStream *js = purple_connection_get_protocol_data(gc);
 	JabberBuddy *jb;
 	JabberBuddyResource *jbr;
-	xmlnode *message;
-	gchar *resource = NULL, *me = NULL,  *who = NULL;
+	xmlnode *message, *child;
+	gchar *resource = NULL, *me = NULL, *who = NULL;
 	jb = jabber_buddy_find(js, to, FALSE);
 	if (!jb) {
 		purple_debug_error("jabber-wb", "Could not find Jabber buddy\n");
-		return NULL;
+		return;
 	}
 	resource = jabber_get_resource(to);
 	jbr = jabber_buddy_find_resource(jb, resource);
 	g_free(resource);
 	if (!jbr) {
 		purple_debug_error("jabber-wb", "Could not find buddy's resource\n");
-		return NULL;
+		return;
 	}
 	who = g_strdup_printf("%s/%s", to, jbr->name);
-	me = g_strdup_printf("%s@%s/%s", js->user->node, js->user->domain, js->user->resource);
+	me = g_strdup_printf("%s@%s/%s", js->user->node, js->user->domain,
+			js->user->resource);
 	message = xmlnode_new("message");
 	xmlnode_set_attrib(message, "type", "chat");
-	xmlnode_set_attrib(message, "id", sid);
+	xmlnode_set_attrib(message, "id", jabber_get_next_id(js));
 	xmlnode_set_attrib(message, "to", who);
 	xmlnode_set_attrib(message, "from", me);
-	xmlnode_new_child(message, "whiteboard");
-	return message;
+	child = xmlnode_new_child(message, "whiteboard");
+	xmlnode_set_attrib(child, "action", action);
+	xmlnode_insert_data(child,data,strlen(data));
+	jabber_send(js, message);
 }
 
-PurpleConnection *jabber_wb_message_get_connection(JabberWbMessage *jwm)
-{
+PurpleConnection *jabber_wb_message_get_connection(JabberWbMessage *jwm) {
 	return jwm->jm->js->gc;
 }
 
-void jabber_wb_message_parse(JabberMessage *jm, xmlnode *packet)
-{
+void jabber_wb_message_parse(JabberMessage *jm, xmlnode *packet) {
 	xmlnode *child;
 	gchar *action;
 	JabberWbMessage *jwm;
 	jwm = g_new0(JabberWbMessage, 1);
 	jwm->jm = jm;
 	child = xmlnode_get_child(packet, "whiteboard");
-	action = (char*)xmlnode_get_attrib(child, "action");
+	action = (char*) xmlnode_get_attrib(child, "action");
 	jwm->data = xmlnode_get_data(child);
-	if (!strcmp(action, "initiate")){
-		jwm->action=JABBER_WB_INITIATE;
-	} else if (!strcmp(action, "accept")){
-		jwm->action=JABBER_WB_ACCEPT;
-	} else if (!strcmp(action, "update")){
-		jwm->action=JABBER_WB_UPDATE;
-	} else if (!strcmp(action, "terminate")){
-		jwm->action=JABBER_WB_TERMINATE;
+	if (!strcmp(action, "initiate")) {
+		jwm->action = JABBER_WB_INITIATE;
+	} else if (!strcmp(action, "accept")) {
+		jwm->action = JABBER_WB_ACCEPT;
+	} else if (!strcmp(action, "draw")) {
+		jwm->action = JABBER_WB_DRAW;
+	} else if (!strcmp(action, "clear")) {
+		jwm->action = JABBER_WB_CLEAR;
+	} else if (!strcmp(action, "end")) {
+		jwm->action = JABBER_WB_END;
 	}
-	purple_debug_info("jabber-wb","Sent a whiteboard message, Buddy: %s, Data: %s, Action: %s\n", jwm->jm->to, jwm->data, action);
-	switch(jwm->action){
+	purple_debug_info("jabber-wb",
+			"Sent a whiteboard message, Buddy: %s, Data: %s, Action: %s\n",
+			jwm->jm->to, jwm->data, action);
+	switch (jwm->action) {
 		case JABBER_WB_INITIATE:
 			handle_wb_initiate(jwm);
 			break;
 		case JABBER_WB_ACCEPT:
 			handle_wb_accept(jwm);
 			break;
-		case JABBER_WB_UPDATE:
+		case JABBER_WB_DRAW:
+			//jabber_wb_command_got_draw(jwm);
 			break;
-		case JABBER_WB_TERMINATE:
+		case JABBER_WB_CLEAR:
+			//handle_wb_got_clear(jwm);
+			break;
+		case JABBER_WB_END:
 			break;
 	}
 }
-void jabber_wb_start(PurpleWhiteboard *wb)
-{
+void jabber_wb_start(PurpleWhiteboard *wb) {
 	purple_debug_info("jabber-wb", "calling the jabber wb start function\n");
-	purple_whiteboard_start(wb);	/* Builds the UI, in place for POC */
+	purple_whiteboard_start(wb); /* Builds the UI, in place for POC */
 }
